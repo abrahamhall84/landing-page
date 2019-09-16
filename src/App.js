2cde5ac290492b4b0cd72b4362d4783154cc260a
abrahamhall84 <abrahamhall84@gmail.com>
2019-09-16 13:10:56 -0700

    setting up landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { tsConstructorType } from '@babel/types';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Landing Page E-Commerce Site</h1>
+      <h2>Sign Up</h2>
+      <p>get on our list</p>
+      <h5>be connected</h5>
+      <form>  
+          <input type='text' name='firstname' placeholder='please enter your first name' value='' /> <br />
+          <input type='text' name='email' value='' placeholder='please enter your valid email' /> <br />
+          <input type='button' value='Submit' />
+      </form>
+      <div>
+        <h2>Thank You, Name</h2>
+        <p>for staying connected to us</p>
+      </div>
     </div>
   );
 }
